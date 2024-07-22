da10a43d0f40a0a18408ce34a96c2358ed613946
Mohamed Galal <Mohamed Galal@DESKTOP-28D5B9Q>
2024-07-22 20:05:39 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMVCTow.Controllers
+{
+    public class ProductController : Controller
+    {
+        #region MyRegion
+        ////public IActionResult Index()
+        ////{
+        ////    return View();
+        ////}
+        //public ContentResult PrintResult()
+        //{
+        //    ContentResult result = new ContentResult();
+        //    result.Content = "Welcome To Home Page";
+        //    return result;
+        //}
+        //public JsonResult PrintJson()
+        //{
+        //    JsonResult result = new JsonResult(new { Id = 1, Name = "Mohamed" });
+        //    return result;
+        //} 
+        #endregion
+
+        public ViewResult PrintView()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "View1";
+            return result;
+        }
+    }
+}
